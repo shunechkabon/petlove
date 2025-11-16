b4d2b570c94d6601a3098390ce4133a350e93ccf
shunechkabon <bondaruk.aleksandra92@gmail.com>
2025-11-16 19:37:03 +0200

    update HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,10 @@
 import s from "./HomePage.module.css";
+import homeDesktop1x from "../../assets/images/home-desktop.jpg";
+import homeDesktop2x from "../../assets/images/home-desktop@2x.jpg";
+import homeTablet1x from "../../assets/images/home-tablet.jpg";
+import homeTablet2x from "../../assets/images/home-tablet@2x.jpg";
+import homeMobile1x from "../../assets/images/home-mobile.jpg";
+import homeMobile2x from "../../assets/images/home-mobile@2x.jpg";
 
 const HomePage = () => {
     return (
@@ -15,20 +21,20 @@ const HomePage = () => {
 
                 <picture>
                     <source
-                        srcSet="/src/assets/images/home-desktop.jpg 1x, /src/assets/images/home-desktop@2x.jpg 2x"
+                        srcSet={`${homeDesktop1x} 1x, ${homeDesktop2x} 2x`}
                         media="(min-width: 1280px)"
                     />
                     <source
-                        srcSet="/src/assets/images/home-tablet.jpg 1x, /src/assets/images/home-tablet@2x.jpg 2x"
+                        srcSet={`${homeTablet1x} 1x, ${homeTablet2x} 2x`}
                         media="(min-width: 768px)"
                     />
                     <source
-                        srcSet="/src/assets/images/home-mobile.jpg 1x, /src/assets/images/home-mobile@2x.jpg 2x"
+                        srcSet={`${homeMobile1x} 1x, ${homeMobile2x} 2x`}
                         media="(max-width: 767px)"
                     />
                     <img
                         className={s.image}
-                        src="/src/assets/images/home-desktop.jpg"
+                        src={homeDesktop1x}
                         alt="Happy pet and owner"
                     />
                 </picture>
